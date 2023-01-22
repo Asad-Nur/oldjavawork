cf40e8c8e3b4388c113eb6f7ec5e6fbe62a844e6
Asad-Nur <asad.nur@seattlecolleges.edu>
2023-01-21 18:29:49 -0800

    finish

--- src/modules/iterative/SearchModule.java
+++ src/modules/iterative/SearchModule.java
@@ -8,15 +8,40 @@ package modules.iterative;
 public class SearchModule {
 
     //TODO: Complete this search algorithm to find an element location is in array
-    //      use while loops here test
+    //      use while loops here
+
+
     public static int binarySearch(int[] data, int target){
         //TODO: update with search algorithm
+        int low;
+        int mid;
+        int high;
+
+        low = 0;
+        high = data.length - 1;
+
+        while (high >= low) {
+            mid = (high + low) / 2;
+            if (data[mid] < target) {
+                low = mid + 1;
+            } else if (data[mid] > target) {
+                high = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+
         return 0;
     }
 
 
     //TODO: Complete this search algorithm to check if an element is in array
-    public static boolean contains(int[] data, int target) {
+   public static boolean contains(int[] data, int target) {
+       for (int datum : data) {
+           if (datum == target) {
+               return true;
+           }
+       }
         return false;
     }
 
@@ -24,6 +49,12 @@ public class SearchModule {
     //TODO: Complete this search algorithm to find an element location is in array
     //      use for loops here
     public static int indexOf(int[] data, int target) {
+        int i;
+        for( i = 0; i < data.length; i++){
+            if(data[i] == target){
+                return i;
+            }
+            }
         return 0;
     }
 
