749df1badc0162dd32941b9db9eeaf7614553a22
Asad-Nur <asad.nur@seattlecolleges.edu>
2023-01-30 10:49:54 -0800

    complete

--- src/modules/RecModule.java
+++ src/modules/RecModule.java
@@ -3,23 +3,44 @@ package modules;
 public class RecModule {
 
     public static int fac(int n) {
-        //TODO: COMPLETE BODY
-        return 0;
+        if (n == 0) {
+            return 1;
+        }
+        else {
+            return n * fac(n - 1);
+        }
+
     }
 
     public static boolean isPalindrome(String s) {
-        //TODO : COMPLETE BODY
-        return false;
+        if (s.length() <=1){
+            return true;
+        }
+        else if (s.charAt(0) != s.charAt(s.length()-1)) {
+            return false;
+        }
+        else{
+            return isPalindrome(s.substring(1,s.length() -1));
+        }
     }
 
     public static double pow(double x, int n) {
-        //TODO: COMPLETE BODY
-        return 0;
+        if (n == 0) {
+           return 1;
+        }
+        else {
+            return x * pow(x, n - 1);
+        }
+
     }
 
     public static int sum(int n) {
-        //TODO : COMPLETE BODY
-        return 0;
+        if (n == 1) {
+            return 1;
+        }
+        else {
+            return n + sum(n - 1);
+        }
     }
 
 }
