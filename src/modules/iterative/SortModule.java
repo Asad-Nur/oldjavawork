c0d7503eac33ea7af6acaebfc25c2bbcfe2c175f
Asad-Nur <asad.nur@seattlecolleges.edu>
2023-01-30 10:51:06 -0800

    complete

--- src/modules/iterative/SortModule.java
+++ src/modules/iterative/SortModule.java
@@ -49,25 +49,6 @@ public class SortModule {
     }
 
 
-    // This is an example of "finding the run time function"
-    // for a maximum value algorithm
-    public static int max(int[] data){
-        int n   = data.length;          // (1)      data size
-        int max = data[0];              // (1)
-
-        //       (1)
-        for(int i = 0; i < n; i++)     // n times
-            // (1  + 1)
-            if (max < data[i])                // (1)
-                max = data[i];
-            // (1)
-        // (1)      terminates loop
-        return max;                     // (1)
-
-        // run time function f(n) = 1 + 1 + 1 + n(2 + 1 + 1)
-        //                   f(n) = 4n + 5
-    }
-
 
     //TODO: Update/Complete the following selectionSort algorithm
     //      using for loops and swap method (see below)
