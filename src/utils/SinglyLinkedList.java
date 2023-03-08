f756af5d0e2edec61e39415c1ed2224b3ed25a51
Asad-Nur <asad.nur@seattlecolleges.edu>
2023-03-08 01:01:46 -0800

    new add

---
+++ src/utils/SinglyLinkedList.java
@@ -0,0 +1,87 @@
+package utils;
+
+public class SinglyLinkedList<T> {
+
+    Node<T> first;  //reference to first item in list
+    int     size;   //counter
+
+    public SinglyLinkedList(){
+        first = null;
+        size  = 0;
+    }
+
+    public boolean add(T item){
+        append(item);    // operation to append to back
+        size++;          // update counter with added node
+
+        return true;
+    }
+
+
+    // add items to the back of the SLL
+    private void  append(T item){
+        Node<T> currentLast = node(size - 1);   // find the last location
+        Node<T> newNode     = new Node<>(item);       // made new node with item to append
+
+        if(isEmpty())
+            first = newNode;                            // assign first reference to new node
+        else
+            currentLast.next    = newNode;              // attach new node to the back
+    }
+
+
+    public boolean isEmpty(){
+        return first == null && size == 0;
+    }
+
+    // private helper class counter for finding the node address at
+    // the index location in SLL
+    private Node<T> node(int index){
+        Node<T> current = first;
+
+        for (int i = 0; i < index; i++) {
+            current = current.next;
+        }
+        return current;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public String toString(){
+        if(isEmpty()){
+            return "[]";
+        }else{
+            StringBuilder result = new StringBuilder("[" + first.data);
+
+            for (Node<T> node = first.next; node != null; node = node.next){
+                result.append(", ").append(node.data);
+            }
+
+            return result + "]";
+        }
+
+    }
+
+
+    // This is an inner class that is decoupled from outer class.
+    // The purpose is primarily for storage of data and next reference in linked
+    // sequence.
+    private static class Node<E>{
+        E data;             // data storage
+        Node<E> next;       // self-reference
+
+        private Node(E data){
+            this(data, null);
+        }
+
+        private Node(E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+        }
+
+    }
+
+
+}
