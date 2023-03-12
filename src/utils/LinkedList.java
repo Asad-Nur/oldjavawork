bef77cd8f2a0c925c95de381551542c1f7cc2f33
Asad-Nur <asad.nur@seattlecolleges.edu>
2023-03-11 16:06:07 -0800

    complete

--- src/utils/LinkedList.java
+++ src/utils/LinkedList.java
@@ -12,7 +12,37 @@ package utils;
  *        3. Use JavaDoc Comments above each method.
  *****************************************************************/
 
-public class LinkedList {
+public class LinkedList<E> {
 
     //TODO : Complete Body with Data Fields, Methods and Classes
+    Node<E> first; //reference to first node in list
+    Node<E> last; //reference to last node in list
+    int size; //counter of index in list
+    public LinkedList(){
+        first   = null;
+        last    = null;
+        size    = 0;
+    }
+
+
+    // This is an inner class that is decoupled from outer class.
+    // The purpose is primarily for storage of data, prev and next
+    // reference in linked sequence.
+    private static class Node<E>{
+        E data;             // data storage
+        Node<E> next;       // self-reference after current node
+        Node<E> prev;       // self-reference before current node
+
+        private Node(Node<E> prev, E data){
+            this(prev, data, null);
+        }
+
+        private Node(Node<E> prev, E data, Node<E> next){
+            this.data = data;
+            this.next = next;
+            this.prev = prev;
+        }
+
+    }
+
 }
