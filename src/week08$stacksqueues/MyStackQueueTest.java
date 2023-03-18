40906b06417a9f75d6a78c7bd1381dc5991a88b5
Asad Nur <assad.nur@seattlecolleges.edu>
2023-03-18 02:50:44 -0700

    complete

--- src/week08$stacksqueues/MyStackQueueTest.java
+++ src/week08$stacksqueues/MyStackQueueTest.java
@@ -8,6 +8,37 @@ import utils.MyStack;
 
 public class MyStackQueueTest {
 
+
+    public int removeMin(MyStack<Integer> s) {
+        Queue<Integer> q = new LinkedList<Integer>();
+        int min = s.peek();
+
+        while(!s.isEmpty()) {
+            int n = s.pop();
+
+            if(n < min)
+                min = n;
+
+            q.add(n);
+        }
+
+        while(!q.isEmpty()) {
+            int n = q.remove();
+
+            if(n > min)
+                s.push(n);
+        }
+
+        while(!s.isEmpty())
+            q.add(s.pop());
+
+        while(!q.isEmpty())
+            s.push(q.remove());
+
+        return min;
+    }
+
+
     public static void intro(){
         System.out.println();
         System.out.println();
@@ -210,22 +241,220 @@ public class MyStackQueueTest {
 
 
     public static void myQueueVersionTest(){
+
         //TODO : Create println tests for each of your MyQueue methods
         System.out.println();
         System.out.println("==========  M Y   Q U E U E  ==========");
+        System.out.println();
+
+        MyQueue<String> queue  = new MyQueue<>();
+        MyQueue<String> queue1 = new MyQueue<>();
+        MyQueue<String> queue2 = new MyQueue<>();
+
+        queue.add("Red");
+        queue.add("Orange");
+        queue.add("Yellow");
+        queue.add("Green");
+        queue.add("Blue");
+        queue.add("Indigo");
+        queue.add("Violet");
+        System.out.println(queue);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+        //test remove
+        System.out.println("----------- Test remove/ size /isEmpty -----------");
+        System.out.println();
+        System.out.println("----------original-----------");
+        System.out.println("size: " + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println();
+        System.out.println("----------1st remove-----------");
+        System.out.println("Remove element: " + queue.remove());
+        System.out.println("isEmpty? "        + queue.isEmpty());
+        System.out.println("size: "           + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------2nd remove-----------");
+        System.out.println("Remove element: " + queue.remove());
+        System.out.println("isEmpty? "        + queue.isEmpty());
+        System.out.println("size: "           + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------3rd remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------4th remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------5th remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------6th remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------7th remove-----------");
+        System.out.println("Remove element: "   + queue.remove());
+        System.out.println("isEmpty? "          + queue.isEmpty());
+        System.out.println("size: "             + queue.size());
+        System.out.println(queue);
+        System.out.println();
+        System.out.println("----------isEmpty() peek -----------");
+        System.out.println("Remove element: " + queue.peek());
+        System.out.println("isEmpty? "        + queue.isEmpty());
+        System.out.println("size: "           + queue.size());
+        System.out.println(queue);
+        System.out.println();
+
+        //test peek
+        System.out.println("----------- Test peek -----------");
+        queue1.add("Red");
+        queue1.add("Orange");
+        queue1.add("Yellow");
+        System.out.println(queue1);
+        System.out.println(queue1.peek());
+        System.out.println();
+
+        //test add
+        System.out.println("----------- Test add and print front and back -----------");
+        queue2.add("Red");
+        queue2.add("Orange");
+        queue2.add("Yellow");
+        System.out.print("Front ");
+        System.out.print(queue2);
+        System.out.println("Back");
+        System.out.println();
+        System.out.println();
     }
 
     public static void myStackVersionTest(){
+
         //TODO : Create println tests for each of your MyStack methods
         System.out.println();
         System.out.println("==========  M Y   S T A C K  ==========");
+        MyStack<String> stack = new MyStack<>();
+        MyStack<String> stack1 = new MyStack<>();
+
+        System.out.println();
+        System.out.println();
+
+        //test push
+        System.out.println("----------- \t Test push \t -----------");
+        System.out.println(stack.push("Red"));
+        System.out.println(stack.push("Orange"));
+        System.out.println(stack.push("Yellow"));
+        System.out.println(stack.push("Green"));
+        System.out.println(stack.push("Blue"));
+        System.out.println(stack.push("Indigo"));
+        System.out.println(stack.push("Violet"));
+        System.out.println(stack);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+
+        // test pop
+        System.out.println("----------- \t Test pop \t -----------");
+        System.out.println();
+        System.out.println("----------original-----------");
+        System.out.println("size: " + stack.size());
+        System.out.println(stack);
+        System.out.println();
+        System.out.println("----------1st pop-----------");
+        System.out.println("Pop element: "  + stack.pop());
+        System.out.println("isEmpty? "      + stack.isEmpty());
+        System.out.println("size: "         + stack.size());
+        System.out.println(stack);
+        System.out.println();
+        System.out.println("----------2nd pop-----------");
+        System.out.println("Pop element: "  + stack.pop());
+        System.out.println("isEmpty? "      + stack.isEmpty());
+        System.out.println("size: "         + stack.size());
+        System.out.println(stack);
+        System.out.println();
+        System.out.println("----------3rd pop-----------");
+        System.out.println("Pop element: "  + stack.pop());
+        System.out.println("isEmpty? "      + stack.isEmpty());
+        System.out.println("size: "         + stack.size());
+        System.out.println(stack);
+        System.out.println();
+        System.out.println("----------4th pop-----------");
+        System.out.println("Pop element: "  + stack.pop());
+        System.out.println("isEmpty? "      + stack.isEmpty());
+        System.out.println("size: "         + stack.size());
+        System.out.println(stack);
+        System.out.println();
+        System.out.println("----------5th pop-----------");
+        System.out.println("Pop element: "  + stack.pop());
+        System.out.println("isEmpty? "      + stack.isEmpty());
+        System.out.println("size: "         + stack.size());
+        System.out.println(stack);
+        System.out.println();
+        System.out.println("----------6th pop-----------");
+        System.out.println("Pop element: "  + stack.pop());
+        System.out.println("isEmpty? "      + stack.isEmpty());
+        System.out.println("size: "         + stack.size());
+        System.out.println(stack);
+        System.out.println();
+        System.out.println("----------7th pop-----------");
+        System.out.println("Pop element: "  + stack.pop());
+        System.out.println("isEmpty? "      + stack.isEmpty());
+        System.out.println("size: "         + stack.size());
+        System.out.println(stack);
+        System.out.println();
+        System.out.println();
+
+        //test peek
+        System.out.println("----------- \t Test peek \t -----------");
+        stack1.push("Red");
+        stack1.push("Orange");
+        stack1.push("Yellow");
+        System.out.println(stack1);
+        System.out.println("peek element: " + stack1.peek());
+        System.out.println();
+        System.out.println();
+    }
+    public static void removeMinTest() {
+        MyStack<Integer> stack = new MyStack<Integer>();
+        MyStackQueueTest testObj = new MyStackQueueTest();
+        //test removeMin
+        System.out.println("----------- \t Test removeMin \t -----------");
+        stack.push(2);
+        stack.push(8);
+        stack.push(3);
+        stack.push(11);
+        stack.push(2);
+        stack.push(3);
+        stack.push(2);
+        stack.push(7);
+        stack.push(12);
+        stack.push(4);
+        System.out.println(stack);
+        testObj.removeMin(stack);
+        System.out.println(stack);
+        System.out.println();
+        System.out.println();
     }
 
     public static void main(String[] args) {
         intro();
-        libraryStackVersionTest();
-        libraryQueueVersionTest();
-
+        myQueueVersionTest();
+        myStackVersionTest();
+        removeMinTest();
     }
 
 }
